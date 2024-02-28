62922dc3ebf360167f88df913e93660dc67555c5
PascalGraf95 <pascal.graf@hs-heilbronn.de>
2024-02-28 11:17:53 +0100

    dataset creation update

--- src/timeseries_forecasting/training/angle/beginning/time_series_forecasting.py
+++ src/timeseries_forecasting/training/angle/beginning/time_series_forecasting.py
@@ -1,12 +1,14 @@
+import random
+
 import numpy as np
 import tensorflow as tf
 import os
 from typing import List, Dict
-from keras import backend as K
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, BatchNormalization, Dropout, Bidirectional
-from tensorflow.keras.callbacks import TensorBoard
 from tensorflow import keras
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, BatchNormalization, Dropout, Bidirectional
+from keras.callbacks import TensorBoard
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -44,11 +46,29 @@ def get_file_names(folder_path: str) -> List:
     return [os.path.join(folder_path, file) for file in os.listdir(folder_path)]
 
 
-def filter_and_preprocess_data_multiple_series(folder_path: str, sequence_length: int, output_length: int):
+def filter_and_preprocess_data_multiple_series(folder_path: str, sequence_length: int, output_length: int,
+                                               overlap: int = -1):
+    if overlap < 0:
+        overlap = sequence_length + output_length
+
     file_names = get_file_names(folder_path)
 
-    x_list = []
-    y_list = []
+    np.random.seed(42)
+    training_file_names = np.random.choice(file_names, int(0.6*len(file_names)))
+    remaining_file_names = [f for f in file_names if f not in training_file_names]
+
+    validation_file_names = np.random.choice(remaining_file_names, int(0.5*len(remaining_file_names)))
+    test_file_names = [f for f in file_names if f not in validation_file_names]
+
+    x_train_list = []
+    y_train_list = []
+
+    x_val_list = []
+    y_val_list = []
+
+    x_test_list = []
+    y_test_list = []
+
     for file in file_names:
         data = load_data_from_csv(file)
         if len(data) >= sequence_length + output_length:
@@ -68,20 +88,34 @@ def filter_and_preprocess_data_multiple_series(folder_path: str, sequence_length
             numpy_data = np.column_stack((sin_angle1, cos_angle1, sin_angle2, cos_angle2))
             # print("DATA SHAPE: {}".format(numpy_data.shape))
 
-            x_sample = numpy_data[:sequence_length]
-            y_sample = numpy_data[sequence_length:sequence_length + output_length].flatten()
+            for i in range(0, numpy_data.shape[0] - sequence_length - output_length, overlap):
+                x_sample = numpy_data[i:i+sequence_length]
+                y_sample = numpy_data[i+sequence_length:i+sequence_length + output_length].flatten()
 
-            x_list.append(x_sample)
-            y_list.append(y_sample)
+                if file in training_file_names:
+                    x_train_list.append(x_sample)
+                    y_train_list.append(y_sample)
+                elif file in validation_file_names:
+                    x_val_list.append(x_sample)
+                    y_val_list.append(y_sample)
+                else:
+                    x_test_list.append(x_sample)
+                    y_test_list.append(y_sample)
 
-    x = np.array(x_list)
-    y = np.array(y_list)
+    x_train = np.array(x_train_list)
+    y_train = np.array(y_train_list)
 
-    print("DATA SHAPE AFTER: {}, {}".format(x.shape, y.shape))
+    x_val = np.array(x_val_list)
+    y_val = np.array(y_val_list)
 
-    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=1)
-    x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.25,
-                                                        random_state=1)  # 0.25 x 0.8 = 0.2
+    x_test = np.array(x_test_list)
+    y_test = np.array(y_test_list)
+
+    print("DATA SHAPE AFTER: {}, {}".format(x_train.shape, y_train.shape))
+
+    # x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=1)
+    # x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.25,
+                                                            # random_state=1)  # 0.25 x 0.8 = 0.2
     return x_train, x_val, x_test, y_train, y_val, y_test
 
 
@@ -102,40 +136,27 @@ class RNNModel:
         model_input = keras.layers.Input((inputs, self.input_shape))
         x = model_input
 
-        # First LSTM layer
-        x = LSTM(units[0], activation='relu', return_sequences=True)(x)
-        x = BatchNormalization()(x)
-        x = Dropout(0.2)(x)
-
-        # Second LSTM layer
-        x = LSTM(units[1], activation='relu', return_sequences=True)(x)
-        x = BatchNormalization()(x)
-        x = Dropout(0.2)(x)
-
-        # Third LSTM layer
-        x = LSTM(units[2], activation='relu', return_sequences=True)(x)
-        x = BatchNormalization()(x)
-        x = Dropout(0.2)(x)
-
-        # Fourth LSTM layer
-        x = LSTM(units[3], activation='relu', return_sequences=False)(x)
-        x = BatchNormalization()(x)
-        x = Dropout(0.2)(x)
+        for i in range(layers):
+            print(i)
+            # First LSTM layer
+            x = LSTM(units[i], activation='relu', return_sequences=(i < layers-1))(x)
+            x = BatchNormalization()(x)
+            x = Dropout(0.25)(x)
 
         # Dense layers
-        x = Dense(2 * units[0], activation='linear')(x)
+        # x = Dense(2 * units[-1], activation='linear')(x)
         model_output = Dense(self.input_shape * outputs)(x)
 
         self.model = keras.Model(inputs=model_input, outputs=model_output)
         self.model.summary()
 
-        self.model.compile(optimizer='adam', loss='mse', metrics=['mse', rmse])
+        self.model.compile(optimizer='adam', loss='mse')
 
     def train_model(self, x_train, x_val, y_train, y_val, epochs=100, batch_size=32):
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S_rnn_model")
-        path_checkpoint = f"./model_checkpoints/{timestamp}_rnn_model.h5"
+        path_checkpoint = f"./model_checkpoints/{timestamp}_rnn_model.keras"
         path_tensorboard = f"./logs/{timestamp}_rnn_model"
-        early_stopping = keras.callbacks.EarlyStopping(monitor="val_loss", patience=15)
+        early_stopping = keras.callbacks.EarlyStopping(monitor="val_loss", patience=5)
 
         checkpoint = keras.callbacks.ModelCheckpoint(
             monitor="val_loss",
@@ -199,27 +220,27 @@ class RNNModel:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=".")
     # Modes
-    parser.add_argument('-t', '--train', action=argparse.BooleanOptionalAction, default=False,
+    parser.add_argument('-t', '--train', action=argparse.BooleanOptionalAction, default=True,
                         help="", required=False)
-    parser.add_argument('-e', '--evaluate', action=argparse.BooleanOptionalAction, default=False,
+    parser.add_argument('-e', '--evaluate', action=argparse.BooleanOptionalAction, default=True,
                         help="", required=False)
 
     # Training Parameters
-    parser.add_argument('-seq_len', '--sequence_length', default=100, type=int, help="", required=False)
-    parser.add_argument('-out_len', '--output_length', default=50, type=int, help="", required=False)
-    parser.add_argument('-ov', '--overlap', default=1, type=int, help="", required=False)
+    parser.add_argument('-seq_len', '--sequence_length', default=200, type=int, help="", required=False)
+    parser.add_argument('-out_len', '--output_length', default=100, type=int, help="", required=False)
+    parser.add_argument('-ov', '--overlap', default=-1, type=int, help="", required=False)
     parser.add_argument('-bs', '--batch_size', default=512, type=int, help="", required=False)
     parser.add_argument('-ep', '--epochs', default=400, type=int, help="", required=False)
 
     # Network
     parser.add_argument('-l', '--layers', default=2, type=int, help="", required=False)
-    parser.add_argument('-u', '--units', default=[64, 128, 128, 64], type=list, help="", required=False)
+    parser.add_argument('-u', '--units', default=[64, 128], type=list, help="", required=False)
 
     # Paths
     parser.add_argument('-mp', '--model_path', type=str, default=None,
                         required=False)
     parser.add_argument('-dp', '--folder_path', type=str,
-                        default=r"C:\Users\Marco\dev\git\proj-chaotic-pendulum\src\timeseries_forecasting\data\processed",
+                        default=r"..\..\..\data\processed",
                         required=False)
 
     args = parser.parse_args()
@@ -229,7 +250,7 @@ if __name__ == '__main__':
     x_train, x_val, x_test, y_train, y_val, y_test = filter_and_preprocess_data_multiple_series(
         os.path.join(args.folder_path),
         args.sequence_length,
-        args.output_length)
+        args.output_length, overlap=20)
 
     model = RNNModel(args.sequence_length, args.output_length, layers=args.layers, units=args.units,
                      model_path=args.model_path)
